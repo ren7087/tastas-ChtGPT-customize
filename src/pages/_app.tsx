d03c9d975c8604a2a7534988903fba6bf73187b4
ren7087 <wakamakiren@icloud.com>
2023-04-18 12:46:24 +0900

    headコンポーネント設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<RecoilRoot>
-				<Component {...pageProps} />
-			</RecoilRoot>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>SaleScripter</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="This site is useful for creating content tailored for users of free market apps."
+        />
+        <meta
+          name="keywords"
+          content="文章作成, フリマ, mercari, chatGPT, openAI"
+        />
+        <meta name="author" content="ren" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
+  );
 }
