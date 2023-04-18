faeb63db96bdcccc1172163aa1ee0bad9fdecdac
ren7087 <wakamakiren@icloud.com>
2023-04-18 17:20:07 +0900

    callout実装

--- src/components/dnd-kit/components/layout/article/index.tsx
+++ src/components/dnd-kit/components/layout/article/index.tsx
@@ -1,10 +1,14 @@
 import DndWrapper from "../../../components/elements/dnd/dndWrapper";
+import Callout from "../../callout";
 
 const Article = () => {
   return (
-    <div className="flex mx-10 my-10">
-      <DndWrapper />
-    </div>
+    <>
+      <Callout />
+      <div className="flex mx-10 my-10">
+        <DndWrapper />
+      </div>
+    </>
   );
 };
 export default Article;
