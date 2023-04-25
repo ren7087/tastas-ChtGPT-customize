fd3a68e9b3075550b1527cbc2e45db2d96b82812
ren7087 <wakamakiren@icloud.com>
2023-04-25 12:18:12 +0900

    jsx内の軽微な修正

--- src/components/dnd-kit/components/loading/index.tsx
+++ src/components/dnd-kit/components/loading/index.tsx
@@ -7,7 +7,7 @@ const Loading = (props: Props) => {
     <div className="m-auto mt-24 flex justify-center items-center">
       <div className="mr-5">
         <h2 className="font-bold">ローディング中.....</h2>
-        {props.text && <h2 className="font-bold">props.text</h2>}
+        {props.text && <h2 className="font-bold">{props.text}</h2>}
       </div>
       <div className="animate-spin h-20 w-20 bg-blue-300 border-2 border-blue-300 rounded-xl"></div>
     </div>
